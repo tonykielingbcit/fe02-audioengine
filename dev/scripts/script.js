556abbdbb73022e55a869a333d73953bf92587af
Byul Kim <caxbelle@gmail.com>
2023-02-04 01:07:35 -0800

    header part done

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,22 @@
 // Write your JavaScript here...
+const header = document.getElementById("header");
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
+
+const handleClick = (e) => {
+  header.classList.toggle("show");
+  console.log("clicked!");
+};
+
+menuBtn.addEventListener("click", handleClick);
+
+// Hidden mobile menu changing window size
+const mql = window.matchMedia("(min-width: 660px)");
+
+function removeTransition(e) {
+  if (e.matches) {
+    header.classList.remove("show");
+  }
+}
+
+mql.addListener(removeTransition);
