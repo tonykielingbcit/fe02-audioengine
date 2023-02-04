0743c5cf3f026e68c791aaa50e2e6c803b2789de
Byul Kim <caxbelle@gmail.com>
2023-02-04 02:40:28 -0800

    video part done

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -3,11 +3,24 @@ const header = document.getElementById("header");
 const menuBtn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
-const handleClick = (e) => {
-  header.classList.toggle("show");
-  console.log("clicked!");
+const play = document.getElementById("play");
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("close-btn");
+
+const handlePlay = () => {
+  popup.style.visibility = "visible";
 };
 
+const handleClose = () => {
+  popup.style.visibility = "hidden";
+};
+
+closeBtn.addEventListener("click", handleClose);
+play.addEventListener("click", handlePlay);
+
+const handleClick = (e) => {
+  header.classList.toggle("show");
+};
 menuBtn.addEventListener("click", handleClick);
 
 // Hidden mobile menu changing window size
