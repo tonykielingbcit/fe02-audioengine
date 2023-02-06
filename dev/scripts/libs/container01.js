aac93a2ee8ea5962d1052a2b585c49a362b03575
Byul Kim <caxbelle@gmail.com>
2023-02-06 13:49:03 -0800

    carousel initiated

---
+++ dev/scripts/libs/container01.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".banner-slide").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    //appendArrows: $(".banner-slide"),
+  });
+});
